b0a9a3996d729dc2cf6d73c592304d1fa3aff1b2
Jakabos <jumamiller@yahoo.com>
2023-12-12 16:07:24 +0300

    setup

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from todo.models import User
+
 
 # Create your views here.
+class AllUsersView(ListView):
+    model = User
+    template_name = 'todo/users.html'
